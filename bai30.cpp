61298551519095127bf95228a64ea5e6e04ea78c
NGUYEN DUC LONG <138957715+longchubby0210@users.noreply.github.com>
2023-12-23 12:26:23 +0700

    Update bai30.cpp

--- bai30.cpp
+++ bai30.cpp
@@ -1,87 +1,85 @@
 
 #include<iostream>
-#include«istream> 
+#include<fstream> 
 #include<stdio.h>
 using namespace std;
 //Khai bao lop 
-class Queue {
-  private:
-    enum {size=100}; 
-    int Q[size];
-    int E, R;
+class Queue{
+  private: 
+    int Q[N];
+    int F, R;
   public:
-    Queue ();
-    void insert (int x);
-    int remove ();
-    bool isEmpty ;
+    void inSert(int x);
+    int remove();
+    bool isEmpty();
 };
-/==-Chuong trinh chinh==
+//===Chuong trinh chinh===
 int main ()
 {
-//Tao 2 đoi tuơng hang doi, Q1 chua so duong, 22 chua so am
-  Queue 01, 02;
-//Tao doi tuong tep vao 
-  ifstream fin ("daysonguyen-bai30. txt") ;
-//Khai bao bien
+//Tạo 2 đối tượng 
+  Queue Q1, Q2;
+ 
+  ifstream fin ("daysonguyen-bai30.txt") ;
+//Khai báo biến
   int x;
 //Doc day so tu tep 
-  cout<<"Day so nguyen doc duoc tu tep la:\n";
-  while (fin>>x){
-  coutくくxくく” ";
-    if (x>=0) Q1.insert(x) ;
-    else Q2.insert (x) ;
-｝
+  cout<<"Day so nguyen doc tu tep la:\n";
+  while (fin>>x)
+  {
+  cout<<x<<" ";
+    if (x>=0) Q1.inSert(x);
+    else Q2.inSert(x);
+  }
 //Dua ra man hinh va tep 
-  cout<<"\n\nDay cac so duong la: \n";
-  while (!Q1.isEmpty ()){
-    x = Q1.remove ()
-    coutくくxくく”";    
+  cout<<"\nDay cac so duong la:\n";
+  while(!Q1.isEmpty())
+  {
+    x = Q1.remove();
+    cou<<x<<" ";    
   }
   cout<<" \nDay cac so am la: ";
     while (!Q2.isEmpty ()){
-      x = Q2. remove () ;
-      coutくくxくく" ";
+      x = Q2.remove () ;
+      cout<<x<<" ";
     }
-cout<<endl;
-return 0;
-//==-Dinh nghia ham-=
-Queue::Queue():E(-1),R(-1)
+  cout<<endl;
+  return 0;
 }
-
-{
-
+//===Dinh nghia ham===
 void Queue::insert(int x)
-//1. Kiem tra hang doi day
-  if(F==0 && R==size-1 || R+1==F)
+{
+  //Kiem tra hang doi day
+  if(F==0 && R==N-1 || R+1==F)
   {
     cout<<"Hang doi da day. ";
+    return;
   }
-return;
-//2. Tang R len 1 
-if (R==-1) F-R=0;
-else if (R==size-1) R=0;
-else R++;
-//3.Bo sung x vao hang doi tai vi tri R
-Q[R] = x;
-｝
+  //Tang R len 1 
+  if (R==-1) F-R=0;
+  else if (R==N-1) R=0;
+  else R++;
+  //Bo sung x tai vi tri R
+  Q[R] = x;
+}
 
 int Queue::remove()
 {
-//1. Kiem tra rong 
+  //Kiem tra rong 
   if (F==-1)
-  cout<<"Hang doi da rong.";
-  return l;
-//2. Giu lai phan tu lo1 truoc (dau hang doi)
+  {
+    cout<<"Hang doi da rong.";
+    return 1;
+  }
+  
   int tg = Q[F];
-//3. Tang E len 1 
+  //Tang F len 1 
   if (F=-R) F-R=-1;
-else if (F==size-1) F=0;
-else F++;
-
-//4. Tra ve phan tu da loai bo 
+  else if (F==N-1) F=0;
+  else F++;
+ 
   return tg;
 }
 bool Queue::isEmpty()
 {
   return F==-1;
-｝
+}
